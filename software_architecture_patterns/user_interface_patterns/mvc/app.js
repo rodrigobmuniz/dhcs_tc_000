2ffcddd0e5bf39378bcda6ad9ca51001f5c77848
Rodrigo Muniz <rodrigo.b.muniz@ibm.com>
2021-08-20 17:26:49 -0300

    beginning of MVC class

---
+++ software_architecture_patterns/user_interface_patterns/mvc/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { helloWorld, getAllBooks, getBookByName, getBooksByAuthor } = require('./src/books')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send(helloWorld())
+})
+
+app.get('/book', (req, res) => {
+    res.send(getAllBooks())
+})
+
+app.get('/book/:bookName', (req, res) => {
+    res.send(getBookByName(req.params.bookName))
+})
+
+app.get('/book/author/:authorName', (req, res) => {
+    res.send(getBooksByAuthor(req.params.authorName))
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ software_architecture_patterns/user_interface_patterns/mvc/src/books.js
@@ -0,0 +1,56 @@
+class Book {
+    constructor(name, author) {
+        this.name = name;
+        this.author = author;
+    }
+}
+
+class BookStore {
+    constructor() {
+        this.books = [
+            new Book("Trabalho Eficaz com Código Legado", "Michael C. Feathers"),
+            new Book("Refatoração: Aperfeiçoando o Design de Códigos Existentes ", "Martin Fowler"),
+            new Book("Código limpo: Habilidades práticas do Agile Software", "Robert C. Martin"),
+            new Book("Arquitetura limpa: O guia do artesão para estrutura e design de software", "Robert C. Martin"),
+        ]
+    }
+
+    getBooks() {
+        return this.books
+    }
+
+    getBookByName(bookName) {
+        return this.books.find(book => book.name === bookName)
+    }
+
+    getBooksByAuthor(bookAuthor) {
+        return this.books.filter(book => book.author === bookAuthor)
+    }
+
+}
+
+const helloWorld = () => {
+    return 'Hello World!'
+}
+
+const getAllBooks = () => {
+    const bookStore = new BookStore()
+    return bookStore.getBooks()
+}
+
+const getBookByName = (bookName) => {
+    const bookStore = new BookStore()
+    return bookStore.getBookByName(bookName)
+}
+
+const getBooksByAuthor = (authorName) => {
+    const bookStore = new BookStore()
+    return bookStore.getBooksByAuthor(authorName)
+}
+
+module.exports = {
+    helloWorld,
+    getAllBooks,
+    getBookByName,
+    getBooksByAuthor,
+}
